c76a39a68e660008739d10e0808a0a52477fdf1c
SaboorSohaib <abdulsaboorsohaib@gmail.com>
2023-06-06 08:33:21 +0430

    FIX LINTER ERRORS IN MODELS

--- app/models/meeting.rb
+++ app/models/meeting.rb
@@ -1,5 +1,5 @@
 class Meeting < ApplicationRecord
-    belongs_to :coach
+  belongs_to :coach
 
-    validates :name, :time_zone, :week_day, :available_time,  presence: true
+  validates :name, :time_zone, :week_day, :available_time, presence: true
 end
