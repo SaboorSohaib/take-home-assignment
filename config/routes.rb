4dcbec7bb39372c09a9f3e1e459620e4a9797ffa
SaboorSohaib <abdulsaboorsohaib@gmail.com>
2023-06-06 08:34:27 +0430

    UPDATE ROUTES AND ADD DEFAULT VALUE TO TIME ZONE

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,13 @@
 Rails.application.routes.draw do
-  get 'meetings/show'
-  get 'meetings/create'
-  get 'meetings/new'
-  get 'coaches/index'
-  get 'coaches/show'
-  get 'coaches/create'
-  get 'coaches/new'
-  get 'users/index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+  root "users#index"
+  resources :users do
+    resources :coaches, only: [:index, :show, :new, :create] do
+      resources :meetings, only: [:show, :new, :create]
+    end
+  end
+
 end
