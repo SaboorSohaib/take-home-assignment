05677614f64b3dd758a754355afd6950280d1b68
SaboorSohaib <abdulsaboorsohaib@gmail.com>
2023-06-08 08:35:42 +0430

    FIX LINTER ERRORS

--- spec/requests/meetings_spec.rb
+++ spec/requests/meetings_spec.rb
@@ -1,25 +1,24 @@
 require 'rails_helper'
 
-RSpec.describe "Meetings", type: :request do
-  describe "GET /show" do
-    it "returns http success" do
-      get "/meetings/show"
+RSpec.describe 'Meetings', type: :request do
+  describe 'GET /show' do
+    it 'returns http success' do
+      get '/meetings/show'
       expect(response).to have_http_status(:success)
     end
   end
 
-  describe "GET /create" do
-    it "returns http success" do
-      get "/meetings/create"
+  describe 'GET /create' do
+    it 'returns http success' do
+      get '/meetings/create'
       expect(response).to have_http_status(:success)
     end
   end
 
-  describe "GET /new" do
-    it "returns http success" do
-      get "/meetings/new"
+  describe 'GET /new' do
+    it 'returns http success' do
+      get '/meetings/new'
       expect(response).to have_http_status(:success)
     end
   end
-
 end
