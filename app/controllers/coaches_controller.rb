b667e5698c95a20539bd5f6c4110ee03b9607f2f
SaboorSohaib <abdulsaboorsohaib@gmail.com>
2023-06-08 06:35:22 +0430

    ADD QUERIES TO CONTROLLERS

--- app/controllers/coaches_controller.rb
+++ app/controllers/coaches_controller.rb
@@ -1,13 +1,29 @@
 class CoachesController < ApplicationController
   def index
+    @coaches = Coach.all
   end
 
   def show
+    @coach = Coach.find(params[:id])
+    @meeting = @coach.meetings
   end
 
   def create
+    @coach = Coach.new(coach_params)
+    if @coach.save
+      redirect_to @coach
+    else
+      render :new
+    end
   end
 
   def new
+    @coach = Coach.new
+  end
+
+  private
+
+  def coach_params
+    params.require(:coach).permit(:name, :time_zone, :week_day, :available_at, :available_until)
   end
 end
